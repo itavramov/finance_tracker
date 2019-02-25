4815ef8c4fbf658ba6188d972b420e5c3f746b27
Richard Belikov <kambolyy@abv.bg>
2019-02-25 20:17:08 +0200

    adding regAccount func in AccountController

--- controller/AccountController.php
+++ controller/AccountController.php
@@ -1,7 +1,22 @@
 <?php
 
+namespace controller;
+
+use model\Account;
+use model\DAO\AccountDAO;
+
 class AccountController{
     function regAccount(){
+        //TODO REQUEST METHOD VALIDATION
+        $acc_name      = $_POST["acc_name"];
+        $acc_type      = $_POST["acc_type"];
+        $balance       = $_POST["balance"];
+        $acc_currency  = $_POST["acc_currency"];
+        $user_id       = $_SESSION["user_id"];
 
+        $acc = new Account($acc_name,$acc_type,$balance,$acc_currency,$user_id);
+
+        $arr["success"] = AccountDAO::addAccount($acc);
+        echo json_encode($arr);
     }
 }
