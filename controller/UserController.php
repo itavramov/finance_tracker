9ca8d521d45565a25e1478f90f2e172e38540ecf
Borislav <boree.bg@gmail.com>
2019-03-04 20:19:10 +0200

    fixing login

--- controller/UserController.php
+++ controller/UserController.php
@@ -64,7 +64,7 @@ class UserController{
             require_once "view/dashboard.html";
         }
 
-        if (isset($_POST["login"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $email = $_POST["email"];
             $password = $_POST["pass"];
             $db_pass = UserDAO::getPassByEmail($email);
