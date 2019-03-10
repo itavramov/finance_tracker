ec293198015c56bca1a8f75e31fa80efa0768079
Richard Belikov <kambolyy@abv.bg>
2019-03-11 00:35:03 +0200

    adding min and max input value in crypto converter and new feature -> fiat converter

--- view/js/cryptoConverter.js
+++ view/js/cryptoConverter.js
@@ -1,10 +1,10 @@
 function calculateCrypto() {
-    var amount = document.getElementById('converterAmount');
-    var crypto = document.getElementById('cryptoSelect').value;
-    var fiat   = document.getElementById('fiatSelect').value;
-    var result = document.getElementById('converterAmount');
-    var fiatOutput = document.getElementById('fiatOutput');
-    var cryptoField = document.getElementById('cryptoOutput');
+    var amount        = document.getElementById('converterAmount');
+    var crypto        = document.getElementById('cryptoSelect').value;
+    var fiat          = document.getElementById('fiatSelect').value;
+    var fiatOutput    = document.getElementById('fiatOutput');
+    var cryptoField   = document.getElementById('cryptoOutput');
+    var prependAmount = document.getElementById('prependAmount');
     var url;
     switch (crypto) {
         case 'BTC': url ='https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BGN,USD,EUR';
@@ -25,14 +25,28 @@ function calculateCrypto() {
                 return response.json();
             })
             .then(function (myJson) {
-                //cryptoField.innerText = amount.value + " : " + result.value;
-                if (fiat === "BGN"){
-                    cryptoField.innerText = crypto + " : " + amount.value;
-                    fiatOutput.innerText ="BGN: " + result.value*myJson.BGN;
-                } else if(fiat === "USD"){
-                    fiatOutput.innerText ="USD: " + result.value*myJson.USD;
-                }else if(fiat === "EUR"){
-                    fiatOutput.innerText ="EUR: " + result.value*myJson.EUR;
+                //cryptoField.innerText = amount.value + " : " + amount.value;
+                if (amount.value > 999999999){
+                    prependAmount.innerText = amount.value;
+                    cryptoField.innerText   = crypto + " : " + amount.value;;
+                    fiatOutput.innerText    = "Your number is over the limit...";
+                }else if (amount.value < 0) {
+                    prependAmount.innerText = amount.value;
+                    cryptoField.innerText =  crypto + " : " + "( " + amount.value + " )";
+                    fiatOutput.innerText  = "Your number is below the limit...";
+                }
+                else {
+                    if (fiat === "BGN"){
+                        prependAmount.innerText = amount.value;
+                        cryptoField.innerText = crypto + " : " + amount.value;
+                        fiatOutput.innerText ="BGN: " + (amount.value*myJson.BGN).toFixed(2);
+                    } else if(fiat === "USD"){
+                        prependAmount.innerText = amount.value;
+                        fiatOutput.innerText ="USD: " + (amount.value*myJson.USD).toFixed(2);
+                    }else if(fiat === "EUR"){
+                        prependAmount.innerText = amount.value;
+                        fiatOutput.innerText ="EUR: " + (amount.value*myJson.EUR).toFixed(2);
+                    }
                 }
             })
             .catch(function (e) {
@@ -42,14 +56,16 @@ function calculateCrypto() {
 }
 
 function changeCrypto() {
-    var crypto = document.getElementById('cryptoSelect');
-    var cryptoField = document.getElementById('cryptoOutput');
+    var crypto        = document.getElementById('cryptoSelect');
+    var cryptoField   = document.getElementById('cryptoOutput');
+    var prependAmount = document.getElementById('prependAmount');
+    var mainAmount    = document.getElementById('converterAmount').value
 
 
     if (document.getElementById('converterAmount').value === ""){
         cryptoField.innerText = crypto.value + " : 0";
-    }else {
-        cryptoField.innerText = crypto.value + " : " + document.getElementById('converterAmount').value;
+    } else {
+        cryptoField.innerText = crypto.value + " : " + mainAmount;
     }
     calculateCrypto();
 }
